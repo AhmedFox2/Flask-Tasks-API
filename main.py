b72fbcd50d0a7d8ae4ba6a74295c84cbc859bad5
AhmedX <ahmedgalal0222@gmail.com>
2025-05-15 20:23:40 +0300

    Ready To Contuine

---
+++ main.py
@@ -0,0 +1,68 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+import json
+import os
+
+app = Flask(__name__)
+api = Api(app)
+
+python_file = os.path.dirname(__file__)
+
+# قاعدة بيانات وهمية في الذاكرة
+
+def load_json_file():
+    with open(f"{python_file}/tasks.json","r") as f:
+         return json.load(f)
+
+def save_json_file(data):
+    with open(f"{python_file}/tasks.json","w") as f:
+         json.dump(f,data,indent=4)
+
+tasks = load_json_file()
+
+class Main(Resource):
+    def get(self):
+        return {"message": "Welcome"}, 200
+
+# المورد الأساسي
+class Task(Resource):
+    def get(self, task_id):
+        if task_id in tasks:
+            return tasks[task_id], 200
+        return {"message": "Not Found"}, 404
+
+    def delete(self, task_id):
+        if task_id in tasks:
+            del tasks[task_id]
+            return {"message": "Deleted"}, 200
+        return {"message": "Not Found"}, 404
+
+    def put(self, task_id):
+        data = request.get_json()
+        tasks[task_id] = {
+            "title": data.get("title", f"مهمة {task_id}"),
+            "done": data.get("done", False)
+        }
+        return tasks[task_id], 201
+
+# مورد للكل
+class TaskList(Resource):
+    def get(self):
+        return tasks
+
+    def post(self):
+        data = request.get_json()
+        task_id = max(tasks.keys(), default=0) + 1
+        tasks[task_id] = {
+            "title": data.get("title", f"مهمة {task_id}"),
+            "done": data.get("done", False)
+        }
+        return tasks[task_id], 201
+
+# ربط الموارد بالروابط
+api.add_resource(TaskList, '/tasks')
+api.add_resource(Task, '/tasks/<int:task_id>')
+api.add_resource(Main, '/')
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
